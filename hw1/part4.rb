8a135b3ef249b43849086a42b40571de9fdbec79
BenKanouse <kanobt61@gmail.com>
2012-10-12 18:35:58 -0400

    problem 4

--- hw1/part4.rb
+++ hw1/part4.rb
@@ -1,23 +1,27 @@
 class Dessert
+  attr_accessor :name
+  attr_accessor :calories
   def initialize(name, calories)
-    # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
   
   def healthy?
-    # YOUR CODE HERE
+    @calories < 200
   end
   
   def delicious?
-    # YOUR CODE HERE
+    true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    @flavor = flavor
   end
   
   def delicious?
-    # YOUR CODE HERE
+    true unless @flavor == "black licorice"
   end
 end
